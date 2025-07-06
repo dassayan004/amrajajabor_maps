d6814a217a7baa68f8ca737fd7948dd0d6dfacdb
Sayan Das <dassayan004@gmail.com>
2025-07-06 18:00:14 +0530

    .

--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -2,6 +2,8 @@ import 'package:get/get.dart';
 
 import '../modules/home/bindings/home_binding.dart';
 import '../modules/home/views/home_view.dart';
+import '../modules/signup/bindings/signup_binding.dart';
+import '../modules/signup/views/signup_view.dart';
 import '../modules/splash/bindings/splash_binding.dart';
 import '../modules/splash/views/splash_view.dart';
 
@@ -22,6 +24,11 @@ class AppPages {
       name: _Paths.HOME,
       page: () => const HomeView(),
       binding: HomeBinding(),
+    ),
+    GetPage(
+      name: _Paths.SIGNUP,
+      page: () => const SignupView(),
+      binding: SignupBinding(),
     ),
   ];
 }
--- lib/app/routes/app_routes.dart
+++ lib/app/routes/app_routes.dart
@@ -5,10 +5,12 @@ abstract class Routes {
   Routes._();
   static const HOME = _Paths.HOME;
   static const SPLASH = _Paths.SPLASH;
+  static const SIGNUP = _Paths.SIGNUP;
 }
 
 abstract class _Paths {
   _Paths._();
   static const SPLASH = '/';
   static const HOME = '/home';
+  static const SIGNUP = '/signup';
 }
